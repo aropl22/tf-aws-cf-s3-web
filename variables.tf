ba2eb39b813976d86b339c6370c8b770c8994e62
Arek <arobrooklyn@gmail.com>
2024-01-25 21:32:57 -0500

    updated cloudfront configuration

---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "domain_name" {
+  description = "Website Domian Name"
+  type        = string
+}
+
+variable "s3-logs" {
+  description = "Logs bucket"
+  type        = string
+}
+
+variable "aliases" {
+  description = "cf aliases"
+  type        = list(string)
+}
